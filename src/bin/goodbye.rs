f172cdbc6445bc3cca1a8a17158b14963b219e55
Takeshi Matsuda <matsutake.pg@gmail.com>
2021-03-01 00:04:23 +0900

    deploy by serverless-rust

---
+++ src/bin/goodbye.rs
@@ -0,0 +1,16 @@
+use lambda_http::{handler, lambda, Context, IntoResponse, Request};
+use serde_json::json;
+
+type Error = Box<dyn std::error::Error + Sync + Send + 'static>;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    lambda::run(handler(goodbye)).await?;
+    Ok(())
+}
+
+async fn goodbye(_: Request, _: Context) -> Result<impl IntoResponse, Error> {
+    Ok(json!({
+        "message": "Goodbye Serverless and Rust 🌛"
+    }))
+}
---
+++ src/bin/hello.rs
@@ -0,0 +1,16 @@
+use lambda_http::{handler, lambda, Context, IntoResponse, Request};
+use serde_json::json;
+
+type Error = Box<dyn std::error::Error + Sync + Send + 'static>;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    lambda::run(handler(hello)).await?;
+    Ok(())
+}
+
+async fn hello(_: Request, _: Context) -> Result<impl IntoResponse, Error> {
+    Ok(json!({
+        "message": "Hello Serverless and Rust 🌞"
+    }))
+}
