2f7ef3c20dedec9b214c9d787ad295c273e36c9e
Takeshi Matsuda <matsutake.pg@gmail.com>
2021-05-04 18:45:39 +0900

    feat: add dynamodb sample code

--- src/bin/hello.rs
+++ src/bin/hello.rs
@@ -1,4 +1,6 @@
 use lambda_http::{handler, lambda, Context, IntoResponse, Request};
+use rusoto_core::Region;
+use rusoto_dynamodb::{DynamoDb, DynamoDbClient, ListTablesInput};
 use serde_json::json;
 
 type Error = Box<dyn std::error::Error + Sync + Send + 'static>;
@@ -10,6 +12,25 @@ async fn main() -> Result<(), Error> {
 }
 
 async fn hello(_: Request, _: Context) -> Result<impl IntoResponse, Error> {
+    let client = DynamoDbClient::new(Region::ApNortheast1);
+    let list_tables_input: ListTablesInput = Default::default();
+
+    match client.list_tables(list_tables_input).await {
+        Ok(output) => match output.table_names {
+            Some(table_name_list) => {
+                println!("Tables in database:");
+
+                for table_name in table_name_list {
+                    println!("{}", table_name);
+                }
+            }
+            None => println!("No tables in database!"),
+        },
+        Err(error) => {
+            println!("Error: {:?}", error);
+        }
+    }
+
     Ok(json!({
         "message": "Hello Serverless and Rust 1.45.2 🌞"
     }))
